8d35e9bfc7baeedc5be5db43718a9c524e28db2a
Monomee <hoangthth12@gmail.com>
2025-07-22 00:06:35 +0700

    base skill system

---
+++ Assets/Scripts/Hoang/RoleConfig.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleConfig : MonoBehaviour
+{
+    string codeName;
+    int level;
+    float moveSpeed;
+    float range;
+    List<string> skillList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
